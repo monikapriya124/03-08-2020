759314ee99ea116caea84ff6d6364eca94f79cd3
Monika_priya <monikapriya0123@gmail.com>
2020-08-16 18:10:07 +0530

    dynamic data added

---
+++ sample.js
@@ -0,0 +1,38 @@
+var d= new Date();
+document.querySelector(".date").innerHTML=d;
+
+//function sample(){
+  //  document.write("Hai");
+//}
+//var x = function(a,b){
+  //  console.log(a*b);
+//}
+//x(4,5)
+
+//var y=(s,r)=> console.log(s*r);
+//y(9,9)
+//var a=54;{
+//    let a=40;
+//    console.log(a);
+//}
+//console.log(a);
+
+//alert("this is a alert message");
+//prompt("enter your name");
+//confirm("are.you.sure?");
+
+//var a="52";
+//var b=52
+//a=Number(a);
+//b=String(b);
+//document.write(a+b);
+
+//var a="lsjd 23 iudreyf"
+//var a=parseInt(a);
+//document.write(a)
+
+var details={
+  name:"Monika Priya",
+  salary:"50k"
+}
+document.write("<h1>"+details.name+"</h1>"+"<h5>"+details.salary+"</h5>")
